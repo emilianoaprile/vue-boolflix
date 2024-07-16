d3140e0b9e12c97f53f06bb2dbff5dfb2ae6b608
EmilianoAprile <emilianoaprile77@icloud.com>
2024-07-16 11:14:02 +0200

    add > films & series votes transformed from 1 -> 10 to 1 -> 5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,31 +3,54 @@
         <div class="container">
             <h1>films</h1>
             <div class="cards">
-                <CardFilm v-for="film in store.films" :key="film.id" :film="film"></CardFilm>
+                <CardFilm v-for="film in transformedFilms" :key="film.id" :film="film"></CardFilm>
             </div>
             <h1>Series</h1>
             <div class="cards">
-                <CardSerie v-for="serie in store.series" :key="serie.id" :serie="serie"></CardSerie>
+                <CardSerie v-for="serie in transformedSeries" :key="serie.id" :serie="serie"></CardSerie>
             </div>
         </div>
     </main>
 </template>
 
 <script>
-import {store} from '../store.js'
+import { store } from '../store.js'
 import CardFilm from './CardFilm.vue';
 import CardSerie from './CardSerie.vue';
-    export default {
-        components: {
-            CardFilm,
-            CardSerie
-        },
-        data() {
-            return {
-                store
-            }
+export default {
+    components: {
+        CardFilm,
+        CardSerie
+    },
+    data() {
+        return {
+            store
         }
+    },
+    computed: {
+        transformedFilms() {
+            // mappo i voti trasformandoli di scala da 1 a 10 -> 1 a 5 arrotondati per eccesso se >= 0.5, per difetto se < 0.5 (es. 2.4 = 2)
+            return this.store.films.map(film => {
+                return {
+                    ...film,
+                    vote: Math.round(film.vote / 2)
+                };
+            });
+        },
+        transformedSeries() {
+            // mappo i voti trasformandoli di scala da 1 a 10 -> 1 a 5 arrotondati per eccesso se >= 0.5, per difetto se < 0.5 (es. 2.4 = 2)
+            return this.store.series.map(serie => {
+                return {
+                    ...serie,
+                    vote: Math.round(serie.vote / 2)
+                };
+            });
+        }
+    },
+    updated() {
+        console.log(this.transformedFilms);
     }
+}
 </script>
 
 <style lang="scss" scoped>
